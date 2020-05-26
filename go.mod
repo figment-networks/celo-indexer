d07355f0a4a1749e7a9313cc456586916666733d
Bartosz Pieńkowski <pienkowb@gmail.com>
2020-05-27 00:13:42 +0200

    Fetch validators using a gRPC proxy

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/golang/protobuf v1.4.2
+	google.golang.org/grpc v1.29.1
+	google.golang.org/protobuf v1.23.0
 )
--- server/server.go
+++ server/server.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+
+	"github.com/figment-networks/celo-indexer/grpc/validator"
 )
 
 func Run() {
@@ -14,8 +18,19 @@ func Run() {
 	router.Run()
 }
 
-func GetValidators(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"validators": []string{},
-	})
+const proxyUrl = "localhost:50051"
+
+func GetValidators(ctx *gin.Context) {
+	conn, err := grpc.Dial(proxyUrl, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		panic("could not connect to gRPC proxy")
+	}
+	defer conn.Close()
+
+	client := validator.NewValidatorServiceClient(conn)
+
+	response, err := client.GetByHeight(context.Background(),
+		&validator.GetByHeightRequest{Height: 0})
+
+	ctx.JSON(http.StatusOK, response)
 }
